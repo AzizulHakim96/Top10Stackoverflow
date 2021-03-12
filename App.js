b9cd756f3e66404d8e8b570ef137770d6cda24f3
AzizulHakim96 <56322456+AzizulHakim96@users.noreply.github.com>
2021-03-12 13:55:42 -0600

    Question and answers fetched

---
+++ App.js
@@ -0,0 +1,90 @@
+var collapse = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var insideContent = this.nextElementSibling;
+    if (insideContent.style.display === "block") {
+      insideContent.style.display = "none";
+    } else {
+      insideContent.style.display = "block";
+    }
+  });
+}
+
+async function opener() {
+    var T = "java"
+    var newT = document.getElementById("query").value;
+    if (newT != null){
+        T = newT
+    }
+    
+    var currentDate = Date.now();
+    currentDate = Math.floor(currentDate / 1000);
+    var aWeekFromNow = currentDate - 604800; //going 168 hours back
+    aWeekFromNow = Math.floor(aWeekFromNow);
+    
+    await top10Questions(1); //10 newest (category1)
+    await top10Questions(2); //10 most voted (category2)
+
+    async function top10Questions(categoryID){
+        var api;
+        if (categoryID === 1){
+            api = 'https://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=' + aWeekFromNow + '&todate=' + currentDate + '&order=desc&sort=creation&tagged='+T+'&site=stackoverflow'
+        } else if(categoryID === 2){
+            api = 'https://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=' + aWeekFromNow + '&todate=' + currentDate + '&order=desc&sort=week&tagged='+T+'&site=stackoverflow'
+        }
+        var req = new Request(api);
+        let json = await fetch(
+            req
+        ).then(function(response){
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error('Something went wrong!');
+        });
+
+        if (json === null){
+            alert('Error')
+        }
+        var questionBodies = json.items;
+        var associatedAnswers;
+
+        var i;
+        for (i = 0; i < questionBodies.length; i++) {
+            questionNumber = i + 1;
+            categoryQ = "category"+categoryID+"Q"+questionNumber;
+            votes = ", Votes: "
+            date = ", Date: "
+            document.getElementById(categoryQ).innerHTML = questionBodies[i].title + 
+            votes.bold()+ questionBodies[i].score +
+            date.bold()+ (new Date(questionBodies[i].creation_date*1000)).toLocaleString('en-GB', { timeZone: 'UTC' }-6);
+            
+            associatedAnswers = await getAnswers(questionBodies[i].question_id);
+            categoryA = "category"+categoryID+"A"+questionNumber;
+            
+            var ans = document.getElementById(categoryA);
+
+            if(associatedAnswers === undefined){
+                ans.innerHTML = "Not answered!";
+            }else{
+                ans.innerHTML = associatedAnswers.body;
+            }
+        }
+        return true;
+    }
+    async function getAnswers(qID){
+        var API = "https://api.stackexchange.com/2.2/questions/" + qID + "/answers?&order=desc&sort=votes&site=stackoverflow&filter=!9_bDE(fI5"
+        let body = await fetch(
+            API
+        ).then(function(response){
+            if (response.ok){
+                return response.json();
+            }
+            throw new Error('Something went wrong!');
+        });
+        return body.items[0];
+    }
+
+};
